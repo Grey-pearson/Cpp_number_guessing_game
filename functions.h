cf6d55b7a094c233c01aef2db570be3d634c3f6f
grey <greytpearson@gmail.com>
2024-06-05 11:26:39 -0600

    Update functions.h

--- functions.h
+++ functions.h
@@ -13,12 +13,12 @@ public:
     void generate_randon_int()
     {
         random_int = (rand() % max_random_int) + 1;
-        cout << "debug " << random_int << endl;
+        // cout << "debug " << random_int << endl;
     };
 
     void ask_for_guess()
     {
-        cout << 'guess a random number between 1 and ' << max_random_int << ": ";
+        cout << "guess a random number between 1 and " << max_random_int << ": ";
         cin >> user_guess;
         // cout << "debug " << user_guess << endl;
         cout << endl;
